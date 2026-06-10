f8b94bac0f6d446821ae98afb53ca805e9630064
Daniel Gyoerffy <judalion777@gmail.com>
2026-06-10 11:15:26 +0200

    add circular integration benchmark scripts and results

---
+++ circle10_par_test.sh
@@ -0,0 +1,20 @@
+TEST_RESULTS_FILE="circle10_par_test_results.txt"
+TIME_RESULTS_FILE="circle10_par_time_results.txt"
+
+rm "$TEST_RESULTS_FILE" 2>/dev/null
+rm "$TIME_RESULTS_FILE" 2>/dev/null
+
+for ((N=1; N<=10000; N*=10))
+do
+    N_PHI=$((N * 6))
+    
+    OUTPUT=$(./gauss2d_circle_integral_par 10 "$N" "$N_PHI")
+    
+    echo -n "$N Hiba: " 
+    echo "$OUTPUT" | grep Hiba | awk '{print $2}' | tee -a "$TEST_RESULTS_FILE"
+    
+    echo -n "Idő: "
+    echo "$OUTPUT" | grep Idő | awk '{print $2}' | tee -a "$TIME_RESULTS_FILE"
+
+    echo ""
+done
---
+++ circle5_seq_test.sh
@@ -0,0 +1,20 @@
+TEST_RESULTS_FILE="circle5_seq_test_results.txt"
+TIME_RESULTS_FILE="circle5_seq_time_results.txt"
+
+rm "$TEST_RESULTS_FILE" 2>/dev/null
+rm "$TIME_RESULTS_FILE" 2>/dev/null
+
+for ((N=1; N<=10000; N*=10))
+do
+    N_PHI=$((N * 6))
+    
+    OUTPUT=$(./gauss2d_circle_integral_seq 5 "$N" "$N_PHI")
+    
+    echo -n "$N Hiba: " 
+    echo "$OUTPUT" | grep Hiba | awk '{print $2}' | tee -a "$TEST_RESULTS_FILE"
+    
+    echo -n "Idő: "
+    echo "$OUTPUT" | grep Idő | awk '{print $2}' | tee -a "$TIME_RESULTS_FILE"
+
+    echo ""
+done
